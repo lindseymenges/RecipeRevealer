e2598c862ce98c671fdde59d78148e01e54b3759
Lindsey Menges <lindseymenges@gmail.com>
2014-05-06 21:48:37 -0700

    Basic styling completed

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,7 @@
 class Upload < ActiveRecord::Base
   belongs_to :user
-  
+
+  # Validation not working ATM, will come back to this later if time
   # validates :name, presence: {message: "Hey, give me a name! How else will you know what you're cooking?"}
 
   mount_uploader :filepath, Uploader
